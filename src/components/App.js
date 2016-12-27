42f8b22dde68ee6af1c69b925b1bd672d8b52bbb
guruahn <guruahn@gmail.com>
2016-12-28 07:40:10 +0900

    Search 컴포넌트 추가.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
+import Search from './Search';
 
 class App extends Component {
 
     render() {
         return(
+          <div>
             <Counter />
+            <Search />
+          </div>
+
         );
     }
 }
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+const propTypes = {
+};
+const defaultProps = {
+};
+class Search extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>Search</div>
+        );
+    }
+}
+Search.propTypes = propTypes;
+Search.defaultProps = defaultProps;
+export default Search;
