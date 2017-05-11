37813efcdb14d3192990b6a70618b329dca4f067
guruahn <guruahn@gmail.com>
2017-05-11 12:28:46 +0900

    for redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import reducers from './app/reducer';
 
 import { Provider } from 'react-redux';
 
-const store = createStore(reducers);
+//const store = createStore(reducers);
+const store = createStore(
+   reducers, /* preloadedState, */
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(
   <Provider store={store}>
