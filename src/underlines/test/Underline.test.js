8ca8584ac6f0723f3c6777a0615f0028b1c8d687
guruahn <guruahn@gmail.com>
2017-07-18 17:35:23 +0900

    add underline.test

---
+++ src/underlines/test/Underline.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import { Provider } from 'react-redux';
+
+import Underline from '../Underline';
+import user from '../../config/authInfoForTest';
+import { dummy } from './MyUnderlines.dummy';
+
+
+
+describe('<Underline />', () => {
+
+  //공통
+  const props = {
+    "underline":dummy.underline
+  }
+
+  it('[shallow] renders without crashing', () => {
+
+    const wrapper = shallow(<Underline {...props} />);
+
+  });
+
+  it('Underline 렌더하면 링크가 하나 있어야 한다', () => {
+    //given
+    //when
+    const wrapper = shallow(<Underline {...props}/> );
+    //console.log(wrapper.debug())
+    //then
+    expect(wrapper.find('[data-name="book-link"]')).to.have.length(1);
+    //expect(title).to.equal("My Underlines");
+  });
+
+  it('Underline 렌더하면 링크 "설문지 목록" 여야 한다', () => {
+    //given
+    //when
+    const wrapper = shallow( <Underline {...props}/> );
+    console.log(wrapper.debug())
+    //then
+    //const title = wrapper.find('h1').text();
+    //expect(title).to.equal("설문지 목록");
+  });
+});
